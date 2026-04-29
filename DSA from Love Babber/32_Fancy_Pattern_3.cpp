abd2d107d1e82a05697f5fd0443446f9b598bc17
vijendra8475 <vijjubca@gmail.com>
2026-04-30 01:53:31 +0530

    32_Fancy_Pattern_3 [ completed ]

--- DSA from Love Babber/32_Fancy_Pattern_3.cpp
+++ DSA from Love Babber/32_Fancy_Pattern_3.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 int main()
 {
-    int n = 5;
+    int n = 7;
 
     for (int i = 0; i < n; i++)
     {
-        int c = i <= n / 2 ? 2 * i : 0;
+        int c = i <= n / 2 ? 2 * i : 2*(n-i-1);
 
         int num = 1;
         for (int j = 0; j <= c; j++)
