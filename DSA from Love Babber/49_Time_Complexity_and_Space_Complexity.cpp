c8d5553f62634793cf15e73748e24023b0846cee
vijendra8475 <vijjubca@gmail.com>
2026-05-07 22:14:15 +0530

    49 Time and Space Complexity [ O(n) O(n^2) ]

---
+++ DSA from Love Babber/49_Time_Complexity_and_Space_Complexity.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n = 5;
+
+    // O(n)
+    for(int i=0; i<n; i++)
+        cout << i+1;
+    cout << endl;
+
+    // O(n^2)
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<=i; j++)
+            cout << j+1;
+        cout << endl;
+    }
+    
+}
