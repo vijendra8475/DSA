fc84ec39f58ecbf911ffc4815b4992e68722f35a
vijendra8475 <vijjubca@gmail.com>
2026-05-07 22:16:46 +0530

    49 Time and Space Complexity [ O(1) ]

--- DSA from Love Babber/49_Time_Complexity_and_Space_Complexity.cpp
+++ DSA from Love Babber/49_Time_Complexity_and_Space_Complexity.cpp
@@ -3,6 +3,11 @@ using namespace std;
 int main() {
     int n = 5;
 
+    // O(1)
+    for(int i=0; i<2; i++) 
+        cout << i*i;
+    cout << endl;
+
     // O(n)
     for(int i=0; i<n; i++)
         cout << i+1;
@@ -14,5 +19,5 @@ int main() {
             cout << j+1;
         cout << endl;
     }
-    
+
 }
