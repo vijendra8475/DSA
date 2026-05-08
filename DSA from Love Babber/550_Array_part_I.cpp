b749638da7a0e077e810199b3f480b66105544ff
vijendra8475 <vijjubca@gmail.com>
2026-05-08 11:56:08 +0530

    50 Array part I [ Initilize and valid/Invalid ]

---
+++ DSA from Love Babber/550_Array_part_I.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a[10];
+    // cout << "\nBase address of array : " << a;
+    // cout << "\nSize of array : " << sizeof(a);
+
+    // valid initilization
+    int b[] = {2,3,4,5,6,7};
+    int c[3] = {1,2,3};
+    float d[5] = {2.3};
+
+    // Invalid 
+    // char name[2] = {'a','b','c'};
+    cout << "\nSize of new array : " << sizeof(name);
+}
