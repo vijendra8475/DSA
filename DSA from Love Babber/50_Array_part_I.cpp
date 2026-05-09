c0a2a0eac714af26d845eb5430d99744a836a46f
vijendra8475 <vijjubca@gmail.com>
2026-05-09 15:16:39 +0530

    50 Array part I [  Reverse an  Array ]

--- DSA from Love Babber/50_Array_part_I.cpp
+++ DSA from Love Babber/50_Array_part_I.cpp
@@ -1,5 +1,35 @@
 #include<iostream>
 using namespace std;
+
+void count0sand1s(int* a, int n) {
+    int z=0, o=0;
+    for(int i=0; i<n; i++)
+        if(a[i] == 1)
+            o++;
+        else if(a[i] == 0)
+            z++;
+
+    cout << "\nZeros : " << z;
+    cout << "\nOnes : " << o;
+}
+
+void revArray(int* a, int n) {
+    int st = 0, end = n-1;
+    while (st < end) 
+    {
+        int tem = a[st];
+        a[st] = a[end];
+        a[end] = tem;
+        st++;
+        end--;
+    }
+
+    cout << "\nNew Array : ";
+    for(int i=0; i<10; i++)
+        cout << " " << a[i];
+    
+}
+
 int main() {
     int a[10];
     // cout << "\nBase address of array : " << a;
@@ -33,21 +63,27 @@ int main() {
 
 
     // Enter Element which you want top search 
-    int n;
-    cout << "\nEnter Element which you want top search : ";
-    cin >> n;
+    // int n;
+    // cout << "\nEnter Element which you want top search : ";
+    // cin >> n;
 
     // Linear Search
-    for(int i=0; i<10; i++) 
-        if(n == a[i]) {
-            cout << "\nEntered Element is in : " << i+1 << "th position ";
-            break;
-        }
-    
-    cout << "\nElement not found";
+    // for(int i=0; i<10; i++) 
+    //     if(n == a[i]) {
+    //         cout << "\nEntered Element is in : " << i+1 << "th position ";
+    //         break;
+    //     }
+    // cout << "\nElement not found";
+
+
+    // Count 0's and 1's in array
+    // count0sand1s(a,10);
+
+    // Reverse an Array
+    revArray(a, 10);
 
     // Array Output using loop
-    cout << "\nYour Array Elements : " ;
-    for(int i=0; i<10; i++) 
-        cout << a[i] << " ";
+    // cout << "\nYour Array Elements : " ;
+    // for(int i=0; i<10; i++) 
+    //     cout << a[i] << " ";
 }
