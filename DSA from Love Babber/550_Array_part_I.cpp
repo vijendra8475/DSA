8e131dc627461d93ed613b19fb89b7b492893490
vijendra8475 <vijjubca@gmail.com>
2026-05-09 14:48:05 +0530

    50 Ay=rray part I [ Double and Sum of  all Array Elements ]

--- DSA from Love Babber/550_Array_part_I.cpp
+++ DSA from Love Babber/550_Array_part_I.cpp
@@ -12,5 +12,27 @@ int main() {
 
     // Invalid 
     // char name[2] = {'a','b','c'};
-    cout << "\nSize of new array : " << sizeof(name);
+    // cout << "\nSize of new array : " << sizeof(name);
+
+
+    // Array input using loop
+    cout << "\nEnter 10 array elements of interger type : ";
+    for(int i=0; i<10; i++) 
+        cin >> a[i];
+
+    // Double the elements's value
+    for(int i=0; i<10; i++) 
+        a[i] *= 2;
+
+    // sum of all elements of array 
+    int sum = 0;
+    for(int i=0; i<10; i++)
+        sum += a[i];
+    
+    cout << "\nSum of all elememnts of array : " << sum;
+
+    // Array Output using loop
+    // cout << "\nYour Array Elements : " ;
+    // for(int i=0; i<10; i++) 
+    //     cout << a[i] << " ";
 }
