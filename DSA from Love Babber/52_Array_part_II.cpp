a663d36b6290de4e0a771bb8e426f6e3241f0770
vijendra8475 <vijjubca@gmail.com>
2026-05-10 22:38:49 +0530

    52 Array part II [ allPairs ]

--- DSA from Love Babber/52_Array_part_II.cpp
+++ DSA from Love Babber/52_Array_part_II.cpp
@@ -8,8 +8,18 @@ int findUnique(int *a, int n) {
     return m;
 }
 
+void allPairs(int *a, int n) {
+    cout << "\n\nAll posiible pairs : " ;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++)
+            cout << " [" << a[i] << "," << a[j] << "] ";
+        cout << endl;
+    }
+}
+
 int main() {
     int a[5] = {1,2,3,2,1};
 
-    cout << "\nUnique element : " << findUnique(a, 5);
+    // cout << "\nUnique element : " << findUnique(a, 5);
+    allPairs(a, 5);
 }
