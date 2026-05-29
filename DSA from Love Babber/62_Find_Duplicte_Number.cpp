97b5d599f6835482c6e13ae64b25242ebcddc151
vijendra8475 <vijjubca@gmail.com>
2026-05-29 19:15:43 +0530

    62 Find Duplicate value [ improved ]

--- DSA from Love Babber/62_Find_Duplicte_Number.cpp
+++ DSA from Love Babber/62_Find_Duplicte_Number.cpp
@@ -1,32 +1,39 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-void findDuplicateNumber(vector<int> &a) {
+void findDuplicateNumber(vector<int> &a)
+{
     int n = a.size();
 
     vector<int> d(n, 0);
 
-    for(int i=0; i<n; i++) {
-        if(d[a[i]] == 0)
+    for (int i = 0; i < n; i++)
+    {
+        if (d[a[i]] == 0)
             d[a[i]] = 1;
         else
             cout << "\nDuplicate : " << a[i];
     }
 }
 
-void another(vector<int> &a) {
+void another(vector<int> &a)
+{
     int n = a.size();
 
-    for(int i=0; i<n; i++) {
-        if(a[a[i]] > 0) 
-            a[a[i]] = -a[a[i]];
+    for (int i = 0; i < n; i++)
+    {
+        int idx = a[i] > 0 ? a[i] : -a[i];
+        if (a[idx] > 0)
+            a[idx] = -a[idx];
         else
-            cout << "\n" << a[i];
+            cout << "\n"
+                 << idx;
     }
 }
 
-int main() {
-    vector<int> a = {1,3,4,4,2};
+int main()
+{
+    vector<int> a = {1, 3, 4, 4, 2};
     another(a);
 }
