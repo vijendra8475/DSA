d02b489df077c1f5caf923fd74ad37f71d01cb1a
vijendra8475 <vijjubca@gmail.com>
2026-05-30 18:57:22 +0530

    63 Find Missing Element [ complete ]

--- DSA from Love Babber/63_Find_Missing_Element.cpp
+++ DSA from Love Babber/63_Find_Missing_Element.cpp
@@ -30,12 +30,26 @@ void findElement_sortingSwappingMethod(vector<int> &a)
 {
     int n = a.size();
 
-    
+    int i = 0;
+    while (i < n)
+    {
+        int idx = a[i]-1;
+        if(a[i] != a[idx]) {
+            swap(a[i], a[idx]);
+        }
+        else {
+            i++;
+        }
+    }
+
+    for(int i=0; i<n; i++)
+        if(a[i] != i+1)
+            cout << "\nMissing : " << i+1;
 }
 
 int main()
 {
-    vector<int> a = {1, 3, 3, 3, 1};
+    vector<int> a = {1, 3, 5, 3, 4};
     findElement_sortingSwappingMethod(a);
     print(a);
 }
